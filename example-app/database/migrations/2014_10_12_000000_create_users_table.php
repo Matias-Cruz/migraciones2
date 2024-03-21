52c4270c8099216ae985c71afbf825b6bbc6dccd
Matias-Cruz <matiassenati1006@gmail.com>
2024-03-21 14:52:20 -0500

    Actualizado create_users_table

--- example-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ example-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('username');
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
