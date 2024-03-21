ef678ba1117a09e3f8021de1b8af6330448a803e
Matias-Cruz <matiassenati1006@gmail.com>
2024-03-21 14:30:26 -0500

    Creado migracióon para tabla roles

--- example-app/database/migrations/2024_03_21_180348_create_roles_table.php
+++ example-app/database/migrations/2024_03_21_180348_create_roles_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->integer('role_id');
-            $table->string('username');
-            $table->rememberToken();
+            $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
