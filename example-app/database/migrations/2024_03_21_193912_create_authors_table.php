cf77d5973507f21bf5e39b8aa6bdb666b717d7a1
Matias-Cruz <matiassenati1006@gmail.com>
2024-03-21 14:42:30 -0500

    Actualizado create_authors_table

--- example-app/database/migrations/2024_03_21_193912_create_authors_table.php
+++ example-app/database/migrations/2024_03_21_193912_create_authors_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last-names');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
