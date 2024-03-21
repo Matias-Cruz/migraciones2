463a4e7b987e809a7d54ae0a8d86f215782527e0
Matias-Cruz <matiassenati1006@gmail.com>
2024-03-21 14:35:37 -0500

    Creada migracion users

--- example-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ example-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('username');
             $table->rememberToken();
             $table->timestamps();
